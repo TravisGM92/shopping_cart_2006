2df9cd40da6e7abfb8b04d106d8d75d601df1f60
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-08 09:11:00 -0600

    Pass all previously generated tests

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,20 @@
 class Product
 
-attr_reader :product, :price, :quantity, :product_name
+attr_reader :product, :unit_price, :quantity, :name
 
-  def initialize(product, product_name, price, quantity)
+  def initialize(product, name, unit_price, quantity)
     @product = product
-    @product_name = product_name
-    @price = price
-    @quantity = quantity
+    @name = name
+    @unit_price = unit_price
+    @quantity = quantity.to_i
   end
 
+  def category
+    @product
+  end
+
+  def total_price
+    @unit_price * @quantity
+  end
 
 end
