aef585cf1c8eb690258408dff86ef362becc51a4
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-08 09:15:14 -0600

    Create test format for shopping cart file

--- lib/product.rb
+++ lib/product.rb
@@ -1,12 +1,13 @@
 class Product
 
 attr_reader :product, :unit_price, :quantity, :name
-
+attr_accessor :hoard
   def initialize(product, name, unit_price, quantity)
     @product = product
     @name = name
     @unit_price = unit_price
     @quantity = quantity.to_i
+    @hoard = false
   end
 
   def category
@@ -15,6 +16,14 @@ attr_reader :product, :unit_price, :quantity, :name
 
   def total_price
     @unit_price * @quantity
+  end
+
+  def hoard
+    @hoard = true
+  end
+
+  def is_hoarded?
+    @hoard
   end
 
 end
--- test/product_test.rb
+++ test/product_test.rb
@@ -41,4 +41,23 @@ class ProductTest < Minitest::Test
     assert_equal 37.0, product1.total_price
     assert_equal 9.0, product2.total_price
   end
+
+  def can_it_show_hoard_status
+    # skip
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+
+
+    assert_equal false, product2.is_hoarded?
+  end
+
+  def can_it_can_be_hoarded
+    # skip
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product2.hoard
+
+    assert_equal true, product2.is_hoarded?
+  end
+
 end
